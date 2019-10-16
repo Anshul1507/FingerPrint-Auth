7da2b03795fbc0130b09ec23b0d9c38f23fbb59d
Anshul1507 <ag1507anshul@gmail.com>
2019-10-17 00:01:17 +0530

    Added Crypto layer

--- TVAC/app/src/main/java/com/example/fingerprintauth_tvac/MainActivity.java
+++ TVAC/app/src/main/java/com/example/fingerprintauth_tvac/MainActivity.java
@@ -41,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
     private FingerprintManager fingerprintManager;
     private KeyguardManager keyguardManager;
 
+    private KeyStore keyStore;
+    private Cipher cipher;
+    private String KEY_NAME = "AndroidKey";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,15 +79,78 @@ public class MainActivity extends AppCompatActivity {
                 //Main Access
                 mParaLabel.setText("Place your Finger on Scanner to Access");
 
+                generateKey();
+                if(cipherInit()) {
+                    FingerprintManager.CryptoObject cryptoObject = new FingerprintManager.CryptoObject(cipher);
+
                     FingerprintHandler fingerprintHandler = new FingerprintHandler(this);
 
-                    fingerprintHandler.startAuth(fingerprintManager, null);
-
+                    fingerprintHandler.startAuth(fingerprintManager, cryptoObject);
+                }
             }
         }
     }
 
 
+
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void generateKey() {
+
+        try {
+
+            keyStore = KeyStore.getInstance("AndroidKeyStore");
+            KeyGenerator keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
+
+            keyStore.load(null);
+            keyGenerator.init(new
+                    KeyGenParameterSpec.Builder(KEY_NAME,
+                    KeyProperties.PURPOSE_ENCRYPT |
+                            KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                    .setUserAuthenticationRequired(true)
+                    .setEncryptionPaddings(
+                            KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                    .build());
+            keyGenerator.generateKey();
+
+        } catch (KeyStoreException | IOException | CertificateException
+                | NoSuchAlgorithmException | InvalidAlgorithmParameterException
+                | NoSuchProviderException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public boolean cipherInit() {
+        try {
+            cipher = Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/" + KeyProperties.BLOCK_MODE_CBC + "/" + KeyProperties.ENCRYPTION_PADDING_PKCS7);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            throw new RuntimeException("Failed to get Cipher", e);
+        }
+
+
+        try {
+
+            keyStore.load(null);
+
+            SecretKey key = (SecretKey) keyStore.getKey(KEY_NAME,
+                    null);
+
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+
+            return true;
+
+        } catch (KeyPermanentlyInvalidatedException e) {
+            return false;
+        } catch (KeyStoreException | CertificateException | UnrecoverableKeyException | IOException | NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException("Failed to init Cipher", e);
+        }
+
+    }
 
 
 
